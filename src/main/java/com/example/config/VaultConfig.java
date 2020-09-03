da0de83fbe85c00c53ae123fdcb41e847c3bcdb5
Subrat Behera <skbehera@live.in>
2020-09-03 22:41:07 +0530

    Added method_master in VaultConfig

--- src/main/java/com/example/config/VaultConfig.java
+++ src/main/java/com/example/config/VaultConfig.java
@@ -5,5 +5,13 @@ public class VaultConfig {
 	//Line 1 in master;
 	//Line 2 in master;
 	//Line 3 in master;
+	
+	public void method1_master() {
+		
+	}
+	
+	public void method2_master() {
+		
+	}
 
 }
